5212adcc01ec2363ce52ecef0b6a94d3dae82f92
DanielSzopa <daniel.szopa333@gmail.com>
2023-06-30 20:30:40 +0200

    Create appsettings config

--- IdentityFunction/Program.cs
+++ IdentityFunction/Program.cs
@@ -1,7 +1,16 @@
 using IdentityFunction.Middlewares;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        var configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+        builder
+        .AddJsonFile(configPath, false, true)
+        .AddEnvironmentVariables();
+    })
     .ConfigureFunctionsWorkerDefaults(builder =>
     {
         builder.UseMiddleware<ErrorHandlingMiddleware>();
