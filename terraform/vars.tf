85fb51c201bb3291dd92deadf6d27c0a5a16100f
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-01 19:33:44 +0200

    Add serviceBus namespace, topic and subscriptions

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,6 +4,14 @@ locals {
 }
 
 locals {
-  identity            = "identityservices"
-  resource_group_name = "${local.identity}rg${local.short_location}"
+  identity                  = "identityservices"
+  resource_group_name       = "${local.identity}rg${local.short_location}"
+  servicebus_namespace_name = "${local.identity}sbn${local.short_location}"
+}
+
+locals {
+  topic_name                          = "notyfications"
+  veryficationEmail_subscription_name = "veryficationEmail"
+  newsletter_subscription_name        = "newsletter"
+  newsletter_newsletter_filter_name   = "newsletterFilter"
 }
