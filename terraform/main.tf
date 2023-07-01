7892b396a6dba993d255fc97fac4ad4bb9d952b2
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-02 00:41:16 +0200

    Add storage account, app service and fapp resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,5 +40,40 @@ resource "azurerm_servicebus_subscription_rule" "newsletter_subscription_rule" {
       isNewsletterSubscriber = "True"
     }
   }
+}
 
+resource "azurerm_storage_account" "createUser_fapp_storage_account" {
+  name                     = local.createUser_fapp_storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "createUser_fapp_service_plan" {
+  name                = local.createUser_service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "createUser_fapp" {
+  name                       = local.createUser_fapp_name
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  storage_account_name       = azurerm_storage_account.createUser_fapp_storage_account.name
+  storage_account_access_key = azurerm_storage_account.createUser_fapp_storage_account.primary_access_key
+  service_plan_id            = azurerm_service_plan.createUser_fapp_service_plan.id
+
+  site_config {
+    application_stack {
+      dotnet_version              = "v6.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
+
+  app_settings = {
+    "ServiceBus:ConnectionString" = azurerm_servicebus_namespace.servicebus_namespace.default_primary_connection_string
+  }
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -4,9 +4,12 @@ locals {
 }
 
 locals {
-  identity                  = "identityservices"
-  resource_group_name       = "${local.identity}rg${local.short_location}"
-  servicebus_namespace_name = "${local.identity}sbn${local.short_location}"
+  identity                             = "identityservices"
+  resource_group_name                  = "${local.identity}rg${local.short_location}"
+  servicebus_namespace_name            = "${local.identity}sbn${local.short_location}"
+  createUser_service_plan_name         = "${local.identity}cusp${local.short_location}"
+  createUser_fapp_storage_account_name = "${local.identity}cusa${local.short_location}"
+  createUser_fapp_name                 = "${local.identity}cufapp${local.short_location}"
 }
 
 locals {
