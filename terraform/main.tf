d539a706e7d86dbdd85b50268adf9d4ef88931fd
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-02 12:07:26 +0200

    Move functionApp resources to functionApp module

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,38 +37,13 @@ module "serviceBus" {
   short_location          = local.short_location
 }
 
-resource "azurerm_storage_account" "createUser_fapp_storage_account" {
-  name                     = local.createUser_fapp_storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_service_plan" "createUser_fapp_service_plan" {
-  name                = local.createUser_service_plan_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Windows"
-  sku_name            = "Y1"
-}
-
-resource "azurerm_windows_function_app" "createUser_fapp" {
-  name                       = local.createUser_fapp_name
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = azurerm_resource_group.rg.location
-  storage_account_name       = azurerm_storage_account.createUser_fapp_storage_account.name
-  storage_account_access_key = azurerm_storage_account.createUser_fapp_storage_account.primary_access_key
-  service_plan_id            = azurerm_service_plan.createUser_fapp_service_plan.id
-
-  site_config {
-    application_stack {
-      dotnet_version              = "v6.0"
-      use_dotnet_isolated_runtime = true
-    }
-  }
-
-  app_settings = {
-    "ServiceBus:ConnectionString" = module.serviceBus.servicebus_default_primary_connection_string
-  }
+module "createUser_fapp" {
+  source                                       = "./modules/functionApp"
+  resource_group_name                          = azurerm_resource_group.rg.name
+  resource_group_location                      = azurerm_resource_group.rg.location
+  short_location                               = local.short_location
+  createUser_fapp_storage_account_name         = local.createUser_fapp_storage_account_name
+  createUser_service_plan_name                 = local.createUser_service_plan_name
+  createUser_fapp_name                         = local.createUser_fapp_name
+  serviceBus_default_primary_connection_string = module.serviceBus.servicebus_default_primary_connection_string
 }
---
+++ terraform/modules/functionApp/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "short_location" {
+  type = string
+}
+
+variable "createUser_service_plan_name" {
+  type = string
+}
+
+variable "createUser_fapp_storage_account_name" {
+  type = string
+}
+
+variable "createUser_fapp_name" {
+  type = string
+}
+
+variable "serviceBus_default_primary_connection_string" {
+  type = string
+}
