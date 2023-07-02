10249a72fd9ae6e671933e3e0481b16d12b37c6f
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-02 19:35:38 +0200

    Add newsletter function

---
+++ NotyficationFunction/NewsletterFunction.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using NotyficationFunction.Constants;
+
+namespace NotyficationFunction
+{
+    public class NewsletterFunction
+    {
+        private readonly ILogger<NewsletterFunction> _logger;
+
+        public NewsletterFunction(ILogger<NewsletterFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Newsletter")]
+        public void Run([ServiceBusTrigger(Topic.Notyfication, Subscription.Newsletter, Connection = "ServiceBusConnection")] string mySbMsg)
+        {
+            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
