6826d1b115ab87f20959a3ba6a8a8d590c0d9eba
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-02 19:11:05 +0200

    Create veryficationEmail function

---
+++ NotyficationFunction/VeryficationEmailFunction.cs
@@ -0,0 +1,22 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using NotyficationFunction.Constants;
+
+namespace NotyficationFunction
+{
+    public class VeryficationEmailFunction
+    {
+        private readonly ILogger<VeryficationEmailFunction> _logger;
+
+        public VeryficationEmailFunction(ILogger<VeryficationEmailFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("VeryficationEmail")]
+        public void Run([ServiceBusTrigger(Topic.Notyfication, Subscription.VeryficationEmail, Connection = "")] string mySbMsg)
+        {
+            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
