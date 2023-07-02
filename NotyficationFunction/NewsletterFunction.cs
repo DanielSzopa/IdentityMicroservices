f8ee476895688d6865fd3304cfb464e813db5cf3
DanielSzopa <daniel.szopa333@gmail.com>
2023-07-02 20:20:30 +0200

    Add email sending for newsletter and veryfication

--- NotyficationFunction/NewsletterFunction.cs
+++ NotyficationFunction/NewsletterFunction.cs
@@ -1,22 +1,41 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using NotyficationFunction.Constants;
+using NotyficationFunction.Email;
+using NotyficationFunction.Messages;
+using System.Text.Json;
 
 namespace NotyficationFunction
 {
     public class NewsletterFunction
     {
         private readonly ILogger<NewsletterFunction> _logger;
+        private readonly IEmailSender _emailSender;
 
-        public NewsletterFunction(ILogger<NewsletterFunction> logger)
+        public NewsletterFunction(ILogger<NewsletterFunction> logger, IEmailSender emailSender)
         {
             _logger = logger;
+            _emailSender = emailSender;
         }
 
         [Function("Newsletter")]
-        public void Run([ServiceBusTrigger(Topic.Notyfication, Subscription.Newsletter, Connection = "ServiceBusConnection")] string mySbMsg)
+        public async Task Run([ServiceBusTrigger(Topic.Notyfication, Subscription.Newsletter, Connection = "ServiceBusConnection")] string mySbMsg)
         {
-            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            _logger.LogInformation($"Start processing newsletter action...");
+
+            var user = JsonSerializer.Deserialize<User>(mySbMsg);
+
+            var newsletter = new Newsletter(user.FirstName, user.LastName, user.Email);
+            var isSuccessful = await _emailSender.SendEmail(newsletter);
+
+            if (isSuccessful)
+            {
+                _logger.LogInformation("Successful send email with newsletter");
+            }
+            else
+            {
+                _logger.LogError($"Problem with sending email with newsletter, message: {mySbMsg}");
+            }
         }
     }
 }
--- NotyficationFunction/VeryficationEmailFunction.cs
+++ NotyficationFunction/VeryficationEmailFunction.cs
@@ -1,22 +1,41 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using NotyficationFunction.Constants;
+using NotyficationFunction.Email;
+using NotyficationFunction.Messages;
+using System.Text.Json;
 
 namespace NotyficationFunction
 {
     public class VeryficationEmailFunction
     {
         private readonly ILogger<VeryficationEmailFunction> _logger;
+        private readonly IEmailSender _emailSender;
 
-        public VeryficationEmailFunction(ILogger<VeryficationEmailFunction> logger)
+        public VeryficationEmailFunction(ILogger<VeryficationEmailFunction> logger, IEmailSender emailSender)
         {
             _logger = logger;
+            _emailSender = emailSender;
         }
 
         [Function("VeryficationEmail")]
-        public void Run([ServiceBusTrigger(Topic.Notyfication, Subscription.VeryficationEmail, Connection = "ServiceBusConnection")] string mySbMsg)
+        public async Task Run([ServiceBusTrigger(Topic.Notyfication, Subscription.VeryficationEmail, Connection = "ServiceBusConnection")] string mySbMsg)
         {
-            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            _logger.LogInformation($"Start processing veryficationEmail action...");
+
+            var user = JsonSerializer.Deserialize<User>(mySbMsg);
+            var veryficationEmail = new VeryficationEmail(user.FirstName, user.LastName, user.Email);
+
+            var isSuccessful = await _emailSender.SendEmail(veryficationEmail);
+
+            if (isSuccessful)
+            {
+                _logger.LogInformation("Successful send veryfication email");
+            }
+            else
+            {
+                _logger.LogError($"Problem with sending veryfication email, message: {mySbMsg}");
+            }
         }
     }
 }
